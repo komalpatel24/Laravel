69f047ee06c5f85c28355f7de061b57a0840c45a
komalpatel24 <komalkantariya2001@gmail.com>
2022-05-31 12:00:07 +0530

    contact us page is complete

--- resources/views/contactus.blade.php
+++ resources/views/contactus.blade.php
@@ -54,16 +54,16 @@
            color: white;
         }
         .cc1{
-            height: 30px;
-            width: 1px;
+            height: 4vh;
+            width: 0.1vw;
             position: absolute;
             left:37.29%;
             top: 1%;
             background-color: rgb(255, 255, 255);
         }
         .cc2{
-            height: 30px;
-            width: 1px;
+            height: 4vh;
+            width: 0.1vw;
             position: absolute;
             left:57.50%;
             top: 1%;
@@ -75,11 +75,11 @@
             text-align: center;
            margin-top: 1%;
            margin-bottom: 1%;
-           height: 80px;
+           height: 10%;
             color: white;
         }
         .conimg img{
-            height: 400px;
+            height: 83%;
             width: 100%;
         }
         .conimg{
@@ -104,7 +104,7 @@
         .navigation{
             background-color: rgb(0, 0, 128,0.8);
             text-align: center;
-             height: 40px;
+             height: 20%;
         }
        .add1{
             margin-left: 20px;
@@ -113,33 +113,138 @@
         }
         .add{
             display: flex;
+            margin-left: 7%;
         }
 
         .foo {
-            text-decoration: none;
-           position: relative;
-           left: 110%;
-           bottom: 150%;
-           color: black;
-          
-        }.foo h3{
-            text-align: center;
-            color: black;
+        margin-left: 20%;
+        margin-top: 2%;
+          text-align: center;
         }
+        .foo a{
+            text-decoration:none;
+          color: black;
+          font-size: 18px;
 
-        .add2{
-            height: 80%;
-            width: 30%;
-            position: relative;
-            left: 840px;
-            bottom: 140px;
-            border-radius: 6px;
         }
-        .add2 input{
-            width: 250px;
-            height: 30px;
-            font-size: 15px;
+        .foo i{
+            font-size: 20px;
+            color: blue;
+        }
+        .foo h1{
+            font-size: 33px;
+        }
+        .schedule{
+            background-color:rgb(13, 13, 13,0.8);
+            color: white;
+            height: 47vh;
+            display: flex;
        }
+       .visiting h2{
+            margin-left: 14%;
+       }
+        .visiting span{
+            font-size: 19px;
+       }
+       .visiting{
+           margin-left: 12%;
+           margin-top: 1%;
+       }
+       .batch{
+        margin-left: 17%;
+           margin-top: 1%; 
+       }
+       .batch span{
+        font-size: 19px;
+       }
+       .spoken{
+        margin-left: 17%;
+           margin-top: 1%; 
+       }
+       .spoken span{
+        font-size: 19px;
+       }
+       .inquiry{
+        border-radius: 13px;
+        /* background-color: rgb(0, 179, 179,0.5); */
+        padding: 20px;
+        width: 40vw;
+        position: relative;
+        left: 30%;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+       }
+       .inquiry input[type=submit]:hover {
+            background-color: blue;
+          }   
+          .inquiry input[type=submit] {
+                background-color:gray;
+                color: white;
+                padding: 12px 20px;
+                border: none;
+                border-radius: 4px;
+                cursor: pointer;
+                align-items: center;
+                }
+                .inquiry input[type=text], select, textarea {
+                    width: 100%;
+                    padding: 12px;
+                    border: 1px solid #ccc;
+                    border-radius: 4px;
+                    box-sizing: border-box;
+                    margin-top: 6px;
+                    margin-bottom: 16px;
+                    /* resize: vertical; */
+                    }
+                .footer{
+                    background-color: black;
+                    height: 55vh;
+                    color: white;
+                    display: flex;
+                }
+                .foologo img{
+                    width: 20vw;
+                }
+                .foocour{
+                    position: relative;
+                    left: 8%;
+                    top: 5%;
+                    font-size: 17px;
+                }
+                .foocour a{
+                    text-decoration: none;
+                    color: white;
+                }
+                .line1{
+                    width: 20%;
+                    height: 0.2%;
+                    background-color: white;
+                }
+                .touch{
+                    position: relative;
+                    left: 13%;
+                    top: 5%;
+                }
+                .line2{
+                    width: 20%;
+                    height: 0.2%;
+                    background-color: white;
+                }
+                .link{
+                    position: relative;
+                    top: 5%;
+                    left: 20%;
+                    font-size: 110%;
+                   
+                }
+                .link a{
+                    text-decoration: none;
+                    color: white;
+                }
+                .line3{
+                    width: 40%;
+                    height: 0.2%;
+                    background-color: white;
+                }
     </style>
 </head>
 <body>
@@ -170,38 +275,78 @@
                     </div>
            </div>
 
-     <div class="add"> 
-            <div class="row">
-                <div class="col col-sm-6 m-5">
+          <div class="add"> 
                        <div class="add1">
-                            <h1> <b> Address </b></h2> <br>
-                                <big> <b> Branch 1 : </b></big> <br>
+                                <h1> <b> Address </b></h2> <br>
+                                <big style="color:blue;"> <b> Branch 1 : </b></big> <br> <br>
                                 60, Ishwarkrupa Society, Section 3, 1st Floor, opp. <br>
                                 Archana Vidhya Bhavan, Lambe Hanuman Road, Surat - 395006. <br><br>
-                                <big>  <b> Branch 2 : </b> </big> <br> 
+                                <big style="color:blue;">  <b> Branch 2 : </b> </big> <br> <br>
                                 2nd Floor, Varsha Socity-1, NR Dwarkadish Hospital, <br>
                                 Lambe Hanuman Road, Surat - 395006. <br>
                        </div>
-                </div>
-                <div class="col col-sm-6 m-5">
+                               
                         <div class="foo">
-                            <h3> <b style="font-size: 22px;">Contact Us</b> </h3> <br><br>
-                            <a href="#"><i class="fa-solid fa-phone"></i> +91 812 816 6631</a>  <br> <br>
-                            <a href="hirenunadkat99@gmail.com"> <i class="fa-solid fa-envelope"></i>  anglemultimedia@gmail.com</a> <br> <br>
-                            <a href="https://angelmultimedia.com/"> <i class="fa-brands fa-chrome"></i>  www.angelmultimedia.com</a>
+                            <h1> <b>Contact Us</b> </h1> <br><br> <br>
+                            <!-- <a href="#"><i class="fa-solid fa-phone"></i> +91 812 816 6631</a>  <br> <br> -->
+                            
+                            <a href="#"><i class="fa-solid fa-phone"></i> &nbsp; :  Hiren Sir : +91 812 816 6631</a> <br>
+                            <a href="#">Dhaval Sir : +91 </a> <br> <br>
+                            <!-- <a href="hirenunadkat99@gmail.com"> <i class="fa-solid fa-envelope"></i>  anglemultimedia@gmail.com</a> <br> <br> -->
+                            
+                            <a href="hirenunadkat99@gmail.com">  <i class="fa-solid fa-envelope"></i> &nbsp; : anglemultimedia@gmail.com</a> <br> <br> <br>
+                            <!-- <a href="https://angelmultimedia.com/"> <i class="fa-brands fa-chrome"></i>  www.angelmultimedia.com</a> -->
+                            
+                            <a href="https://angelmultimedia.com/"><i class="fa-brands fa-chrome"></i> &nbsp;: www.angelmultimedia.com</a>
                         </div>
+         </div> <br> <br>
+
+         <div class="schedule">
+                <div class="visiting">
+                    <h2>Visiting  Hours  <br> Time Schedule </h2> <br>
+                    <span>
+                        Mon: 09:00 AM – 10:00 PM <br>
+                        Tue: 09:00 AM – 10:00 PM <br>
+                        Wed: 09:00 AM – 10:00 PM <br>
+                        Thu: 09:00 AM – 10:00 PM    <br>
+                        Fri: 09:00 AM – 10:00 PM <br>
+                        Sat: 09:00 AM – 10:00 PM <br>
+                        Sun: 10:00 AM – 01:00 PM 
+                    </span>
                 </div>
-            </div>
-    </div> 
+                <div class="batch">
+                    <h2>Computer  Courses  <br> &nbsp;&nbsp;Batch  Schedule </h2> <br>
+                    <span>
+                        09:00 AM – 10:30 AM <br>
+                        10:30 AM – 12:00 PM  <br>
+                        12:00 PM – 01:30 PM <br>
+                        01:30 PM – 03:00 PM  <br>
+                        03:00 PM – 04:30 PM  <br>
+                        04:30 PM – 06:00 PM  <br>
+                        06:00 PM – 07:30 PM  <br>
+                        07:30 PM – 09:00 PM  <br>
+                        09:00 PM – 10:00 PM
+                    </span>
+                </div>
+                <div class="spoken">
+                    <h2>Spoken  English  <br> Batch  Schedule </h2> <br>
+                    <span>
+                        12:00 PM – 01:30 PM <br>
+                        07:30 PM – 08:30 PM
+                    </span>
+                </div>
+         </div>
 
-            <div class="add2">
-                        <label for="text">username</label> <br>
-                        <input type="text" placeholder="Name" name="name" autofocus> <br><br>
-                        <label for="text">mobile_no</label> <br>
-                        <input type="text" name="mobile_no" placeholder="Mobile">  <br><br>
-                        <label for="text">Courses</label> <br>
-
-                        <select name="department" class="form-control" style="width: 250px;height:30px;">
+        <br> <br> <br>
+             <div class="inquiry">
+                   <form action="home">
+                        <h1 style="text-align: center;">Inquiry</h1> <br> <br>
+                        <label for="text">Username</label>
+                        <input type="text" placeholder="Name" name="name" autofocus> 
+                        <label for="text">Mobile_no</label> 
+                        <input type="text" name="mobile_no" placeholder="Mobile">  
+                        <label for="text">Courses</label> 
+                        <select name="department" class="form-control">
                                         <option value="" selected disabled>-Choose Courses-</option>
                                         <option value="Full Stake">Full Stake</option>
                                         <option value="Web Designing">a Web Designing</option>
@@ -210,9 +355,62 @@
                                         <option value="Video Editing">Video Editing</option>
                                         <option value="CCC">CCC</option>
                                         <option value="Tally">Tally</option>
-                            </select> <br><br>
+                            </select>
+                            <label for="subject">Subject</label>
+                            <textarea id="subject" name="subject" placeholder="Write something.." style="height:100px"></textarea>
+                            <input type="submit" value="Submit">
+                   </form>
+              </div> <br> <br> <br>
+
+         <div class="footer">
+                <div class="foologo">
+                     <img src="../emoji/logow.png" alt="logo" style="margin-top: 8px;">
+                </div>
+                <div class="foocour">
+                    <h2>See Our Professional Courses</h2> <br>
+                    <div class="line1"></div> <br> 
+                    <ul style="list-style-type:square;">
+                        <li><a href="Web Designing">Web Designing</a></li>
+                        <li><a href="Web Development">Web Development</a></li>
+                        <li><a href="Full Stack Development">Full Stack Development</a></li>
+                        <li><a href="Android Application Development">Android Application Development</a></li>
+                        <li><a href="Digital Marketing">Digital Marketing</a></li>
+                        <li><a href="Graphic Design">Graphic Design</a></li>
+                        <li><a href="CCC">CCC</a></li>
+                        <li><a href="Tally">Tally</a></li>
+                    </ul>
+             </div>
+                <div class="touch">
+                    <h2>Get In Touch</h2> <br>
+                    <div class="line2"></div> <br> 
+                    <br>
+                    <ul style="list-style-type:square;">
+                      <li> <big>  Branch 1 : </big> <br> <br>
+                        60, Ishwarkrupa Society, Section 3, <br> 
+                        1st Floor, opp. Archana Vidhya Bhavan, <br> 
+                        Lambe Hanuman Road, Surat - 395006. </li> <br><br>
+                        <li> <big>   Branch 2 : </big> <br> <br>
+                        2nd Floor, Varsha Socity-1, NR <br>
+                        Dwarkadish Hospital,Lambe Hanuman <br> 
+                        Road, Surat - 395006. </li><br>
+                    </ul>
+                </div>
+                <div class="link">
+                    <h2>Connect </h2> <br>
+                    <div class="line3"></div> <br> 
+                    <br>
+                    <ul style="list-style-type:square;">
+                        <li><a href="">Facebook</a></li>
+                        <li><a href="">Instagram</a></li>
+                        <li><a href="">WhatsApp</a></li>
+                        <li><a href="hirenunadkat99@gmail.com">E-mail</a></li>
+                        <li><a href="">Twitter</a></li>
+                    </ul>
                 </div>
 
-      
+
+     </div>
+
+ </div>   
 </body>
 </html>
